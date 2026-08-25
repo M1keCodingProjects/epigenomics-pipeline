07912d45337d0939c716e6a214b0557be186e3ea
M1keCodingProjects <mikif.2002@gmail.com>
2026-08-26 00:13:51 +0200

    Put MACS2 on multiple threads + print tidy-ups

--- pipeline.sh
+++ pipeline.sh
@@ -71,20 +71,23 @@ REP2="$4"
 CTRL1="$5"
 CTRL2="${6:-}"
 
-echo "Full analysis output available in $OUTPUT"
-echo
+echo "Full analysis output available in $OUTPUT."
 
 # Merge controls if requested
 if "$ARE_CTRLS_MERGED" && [ -n "$CTRL2" ]; then
+    echo
     echo "Merging control files..."
+    
     samtools merge -f -@ 8 ctrl.bam "$CTRL1" "$CTRL2"
+    
     CTRL1="ctrl.bam"
     unset CTRL2
-    echo "Controls have been merged into ctrl.bam" | tee -a "$OUTPUT"
+    
+    echo "Controls have been merged into ctrl.bam." | tee -a "$OUTPUT"
 else
-    echo "Controls have not been merged" | tee -a "$OUTPUT"
+    echo
+    echo "Controls have not been merged." | tee -a "$OUTPUT"
 fi
-echo >> "$OUTPUT"
 
 # Filtering
 BAMS=("$REP1" "$REP2" "$CTRL1")
@@ -93,6 +96,7 @@ BAMS=("$REP1" "$REP2" "$CTRL1")
 FILTERED_NAMES=("filtered1" "filtered2" "filtered_ctrl1")
 [ -n "${CTRL2:-}" ] && FILTERED_NAMES+=("filtered_ctrl2")
 
+echo
 echo "Filtering mapping files..."
 for i in "${!BAMS[@]}"; do
     samtools view -bq 1 -@ 8 "${BAMS[$i]}" > "${FILTERED_NAMES[$i]}.bam"
@@ -101,29 +105,29 @@ done
 
 # Mapping QC
 for i in "${!BAMS[@]}"; do
+    echo | tee -a "$OUTPUT"
     echo "${BAMS[$i]} mapping statistics:" | tee -a "$OUTPUT"
 
     READS_AMT=$(samtools view -c -@ 8 "${BAMS[$i]}")
-    echo "Total reads: $READS_AMT" | tee -a "$OUTPUT"
+    echo "  Total reads: $READS_AMT" | tee -a "$OUTPUT"
 
     MAPPING_AMT=$(samtools view -c -F 4 -@ 8 "${BAMS[$i]}")
-    echo "Mapping reads: $MAPPING_AMT" | tee -a "$OUTPUT"
+    echo "  Mapping reads: $MAPPING_AMT" | tee -a "$OUTPUT"
 
     UMAPPING_AMT=$(samtools view -c -@ 8 "${FILTERED_NAMES[$i]}.bam")
-    echo "Uniquely mapping reads: $UMAPPING_AMT" | tee -a "$OUTPUT"
+    echo "  Uniquely mapping reads: $UMAPPING_AMT" | tee -a "$OUTPUT"
 
     MMAPPING_AMT=$((MAPPING_AMT - UMAPPING_AMT))
-    echo "Multi-mapping reads (mapping but not uniquely): $MMAPPING_AMT" | tee -a "$OUTPUT"
+    echo "  Multi-mapping reads (mapping but not uniquely): $MMAPPING_AMT" | tee -a "$OUTPUT"
 
     MAPPING_F=$(awk "BEGIN {printf \"%.3f\", $MAPPING_AMT/$READS_AMT*100}")
-    echo "Mapping reads as a percentage of total reads: ${MAPPING_F}%" | tee -a "$OUTPUT"
+    echo "  Mapping reads as a percentage of total reads: ${MAPPING_F}%" | tee -a "$OUTPUT"
 
     MMAPPING_F=$(awk "BEGIN {printf \"%.3f\", $MMAPPING_AMT/$MAPPING_AMT*100}")
-    echo "Multi-mapping reads as a percentage of mapping reads: ${MMAPPING_F}%" | tee -a "$OUTPUT"
+    echo "  Multi-mapping reads as a percentage of mapping reads: ${MMAPPING_F}%" | tee -a "$OUTPUT"
 
     echo | tee -a "$OUTPUT"
     samtools flagstat -@ 8 "${BAMS[$i]}"
-    echo
     echo
 done
 
@@ -132,41 +136,42 @@ done
 echo "Calling peaks..."
 if [ -n "${CTRL2:-}" ]; then
     echo "Calling peaks on ${REP1}..."
-    macs2 callpeak -t filtered1.bam -c filtered_ctrl1.bam -q "$FDR_THRESHOLD" -g hs -B --SPMR -n REP1 > macs2_log.txt 2>&1
-    echo | tee -a macs2_log.txt
+    macs2 callpeak -t filtered1.bam -c filtered_ctrl1.bam -q "$FDR_THRESHOLD" -g hs -B --SPMR -n REP1 > macs2_REP1_log.txt 2>&1 &
 
     echo "Calling peaks on ${REP2}..."
-    macs2 callpeak -t filtered2.bam -c filtered_ctrl2.bam -q "$FDR_THRESHOLD" -g hs -B --SPMR -n REP2 >> macs2_log.txt 2>&1
-    echo | tee -a macs2_log.txt
+    macs2 callpeak -t filtered2.bam -c filtered_ctrl2.bam -q "$FDR_THRESHOLD" -g hs -B --SPMR -n REP2 > macs2_REP2_log.txt 2>&1 &
     
     echo "Calling peaks on the 2 replicates merged..."
-    macs2 callpeak -t filtered1.bam filtered2.bam -c filtered_ctrl1.bam filtered_ctrl2.bam -q "$FDR_THRESHOLD" -g hs -B --SPMR -n MERGE >> macs2_log.txt 2>&1
-    echo | tee -a macs2_log.txt
+    macs2 callpeak -t filtered1.bam filtered2.bam -c filtered_ctrl1.bam filtered_ctrl2.bam -q "$FDR_THRESHOLD" -g hs -B --SPMR -n MERGE > macs2_MERGE_log.txt 2>&1 &
 else
     echo "Calling peaks on ${REP1}..."
-    macs2 callpeak -t filtered1.bam -c filtered_ctrl1.bam -q "$FDR_THRESHOLD" -g hs -B --SPMR -n REP1 > macs2_log.txt 2>&1
-    echo | tee -a macs2_log.txt
+    macs2 callpeak -t filtered1.bam -c filtered_ctrl1.bam -q "$FDR_THRESHOLD" -g hs -B --SPMR -n REP1 > macs2_REP1_log.txt 2>&1 &
     
     echo "Calling peaks on ${REP2}..."
-    macs2 callpeak -t filtered2.bam -c filtered_ctrl1.bam -q "$FDR_THRESHOLD" -g hs -B --SPMR -n REP2 >> macs2_log.txt 2>&1
-    echo | tee -a macs2_log.txt
+    macs2 callpeak -t filtered2.bam -c filtered_ctrl1.bam -q "$FDR_THRESHOLD" -g hs -B --SPMR -n REP2 > macs2_REP2_log.txt 2>&1 &
     
     echo "Calling peaks on the 2 replicates merged..."
-    macs2 callpeak -t filtered1.bam filtered2.bam -c filtered_ctrl1.bam -q "$FDR_THRESHOLD" -g hs -B --SPMR -n MERGE >> macs2_log.txt 2>&1
-    echo | tee -a macs2_log.txt
+    macs2 callpeak -t filtered1.bam filtered2.bam -c filtered_ctrl1.bam -q "$FDR_THRESHOLD" -g hs -B --SPMR -n MERGE > macs2_MERGE_log.txt 2>&1 &
 fi
+
+echo "All peak calling jobs started, wait a while..."
+wait
+
 echo
-echo "Check the outcome and logs of the peak calling in the macs2_log.txt file"
-echo
+echo "Check the outcome and logs of the peak calling in the macs2_*_log.txt files."
 
 # R plots
+echo
+echo "Plotting..."
 R < REP1_model.r --vanilla > /dev/null
 R < REP2_model.r --vanilla > /dev/null
 R < MERGE_model.r --vanilla > /dev/null
-echo "R plots are available as pdfs"
+
 echo
+echo "R plots are available as pdfs."
 
 # Computing overlaps
+echo
 echo "Computing overlaps..."
 REP1_COUNT=$(wc -l < REP1_peaks.narrowPeak)
 REP2_COUNT=$(wc -l < REP2_peaks.narrowPeak)
@@ -190,26 +195,32 @@ OVERLAPS=$(wc -l < "${REP_COMP_NAME}_peaks.narrowPeak")
 FRACTION=$(awk "BEGIN {printf \"%.3f\", $OVERLAPS/$SMALL_COUNT}")
 PROXIMITY_FRAC=$(awk "BEGIN {printf \"%.3f\", $SUMMIT_PROX/$SMALL_COUNT}")
 
+echo | tee -a "$OUTPUT"
 echo "${SMALL} (${SMALL_COUNT} peaks) in ${BIG} (${BIG_COUNT} peaks):" | tee -a "$OUTPUT"
 echo "  Summit proximity: $SUMMIT_PROX" | tee -a "$OUTPUT"
 echo "  Overlapping peaks: $OVERLAPS" | tee -a "$OUTPUT"
 echo "  Fraction of overlapping peaks: $FRACTION" | tee -a "$OUTPUT"
 echo "  Summit proximity ratio: $PROXIMITY_FRAC" | tee -a "$OUTPUT"
-echo | tee -a "$OUTPUT"
+
 
 MERGE_COUNT=$(wc -l < MERGE_peaks.narrowPeak)
 OVERLAPS=$(bedtools intersect -a REP1_peaks.narrowPeak -b MERGE_peaks.narrowPeak -u | wc -l)
 FRACTION=$(awk "BEGIN {printf \"%.3f\", $OVERLAPS/$REP1_COUNT}")
+
+echo | tee -a "$OUTPUT"
 echo "REP1 (${REP1_COUNT} peaks) vs MERGE (${MERGE_COUNT} peaks):" | tee -a "$OUTPUT"
 echo "  Overlapping peaks: $OVERLAPS" | tee -a "$OUTPUT"
 echo "  Fraction of overlapping peaks: $FRACTION" | tee -a "$OUTPUT"
-echo | tee -a "$OUTPUT"
+
 
 OVERLAPS=$(bedtools intersect -a REP2_peaks.narrowPeak -b MERGE_peaks.narrowPeak -u | wc -l)
 FRACTION=$(awk "BEGIN {printf \"%.3f\", $OVERLAPS/$REP2_COUNT}")
+
+echo | tee -a "$OUTPUT"
 echo "REP2 (${REP2_COUNT} peaks) vs MERGE (${MERGE_COUNT} peaks):" | tee -a "$OUTPUT"
 echo "  Overlapping peaks: $OVERLAPS" | tee -a "$OUTPUT"
 echo "  Fraction of overlapping peaks: $FRACTION" | tee -a "$OUTPUT"
+
 
 # Comparison with the ENCODE results
 echo
@@ -217,7 +228,6 @@ echo "Comparing with ENCODE results..."
 bedtools sort -i "$ENCODE" > ENCODE_peaks.narrowPeak
 NAMES=("REP1" "REP2" "MERGE" "$REP_COMP_NAME")
 for NAME in "${NAMES[@]}"; do
-    echo | tee -a "$OUTPUT"
     echo "Jaccard index for ${NAME} vs ENCODE:" | tee -a "$OUTPUT"
     JACCARD_INDEX=$(bedtools jaccard -a "${NAME}_peaks.narrowPeak" -b ENCODE_peaks.narrowPeak)
     echo "$JACCARD_INDEX" | column -t | tee -a "$OUTPUT"
@@ -229,6 +239,8 @@ for NAME in "${NAMES[@]}"; do
     elif [ "$NAME" = "$REP_COMP_NAME" ]; then
         INTERSECT_JI="$JACCARD_INDEX"
     fi
+
+    echo | tee -a "$OUTPUT"
 done
 
 # Determine final peak set:
